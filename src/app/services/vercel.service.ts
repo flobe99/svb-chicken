6fe07e620de63a33c34f73e3252e57707116cd7e
Florian Betz <florian.betz@kavo.com>
2025-10-16 13:16:58 +0200

    Revert "add ping to render"
    This reverts commit 4b33d9c018ce6d43d28af1d2c740d7b8001df617.

--- src/app/services/vercel.service.ts
+++
@@ -1,4 +0,0 @@
-export default async function handler(req: any, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; }) {
-    await fetch('https://svb-chicken-backend.onrender.com/');
-    res.status(200).send('Ping gesendet');
-}
