2388dd184bc85ed5dd14a06cd9bfa13c1fe021cc
Florian Betz <florian.betz@kavo.com>
2025-10-31 14:53:50 +0100

    loadingController

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButtons, IonInputPasswordToggle, IonButton, IonMenuButton, IonCard, IonItem, IonLabel, IonCardTitle } from '@ionic/angular/standalone';
+import { LoadingController, IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButtons, IonInputPasswordToggle, IonButton, IonMenuButton, IonCard, IonItem, IonLabel, IonCardTitle } from '@ionic/angular/standalone';
 import { RefreshComponent } from 'src/app/components/refresh/refresh.component';
 import { OrderService } from 'src/app/services/Order.Service';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -18,16 +19,26 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginPage implements OnInit {
   public username: string = ""
   public password: string = ""
-  constructor(private authService: AuthService,private orderService: OrderService, private router: Router) { }
+  constructor(private authService: AuthService, private orderService: OrderService, private router: Router, private loadingController: LoadingController) { }
 
   ngOnInit() {
   }
 
 
-  login() {
+  async login() {
+
+    const loading = await this.loadingController.create({
+      message: 'Einloggen...',
+      spinner: 'crescent',
+      duration: 10000
+    });
+
+    await loading.present();
+
     this.authService.login(this.username, this.password).subscribe({
       next: (response: any) => {
         localStorage.setItem('access_token', response.access_token);
+        loading.dismiss();
         this.router.navigate(['/theke']);
       },
       error: (err) => {
